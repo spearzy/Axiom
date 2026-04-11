7c5f74ba3b210dae65ccee95e9d596b58ac7506a
Ollie <ollie.spear3@gmail.com>
2026-04-11 09:40:37 +0100

    Add string and collection support for custom assertion creation

--- src/Axiom.Assertions/Authoring/AssertionContext.cs
+++ src/Axiom.Assertions/Authoring/AssertionContext.cs
@@ -7,11 +7,21 @@ public static class AssertionContext
     public static AssertionContext<ValueAssertions<T>, T> Create<T>(ValueAssertions<T> assertions)
     {
         ArgumentNullException.ThrowIfNull(assertions);
+        return CreateCore(assertions, assertions.Subject, assertions.SubjectExpression);
+    }
 
-        return new AssertionContext<ValueAssertions<T>, T>(
-            assertions,
-            assertions.Subject,
-            ResolveSubjectLabel(assertions.SubjectExpression));
+    public static AssertionContext<StringAssertions, string?> Create(StringAssertions assertions)
+    {
+        ArgumentNullException.ThrowIfNull(assertions);
+        return CreateCore(assertions, assertions.Subject, assertions.SubjectExpression);
+    }
+
+    private static AssertionContext<TAssertions, TSubject> CreateCore<TAssertions, TSubject>(
+        TAssertions assertions,
+        TSubject subject,
+        string? subjectExpression)
+    {
+        return new AssertionContext<TAssertions, TSubject>(assertions, subject, ResolveSubjectLabel(subjectExpression));
     }
 
     private static string ResolveSubjectLabel(string? subjectExpression)
